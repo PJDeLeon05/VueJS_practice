a6c6aa39ae260d3a12c6899d0f7f9fde609bfbe4
Patrick James O. De Leon <pdeleon@katanatechworks.com>
2021-06-02 19:12:52 +0800

    Random-User-Gen
    Finished the random-user-gen web app

--- VueJS Crash Course 2021/random-user-gen/app.js
+++ VueJS Crash Course 2021/random-user-gen/app.js
@@ -1,7 +1,22 @@
 const app = Vue.createApp({
   data() {
     return {
-      firstName: 'Patrick James'
+      firstName: 'Patrick James',
+      lastName: 'De Leon',
+      email: 'patrickjamesdeleon@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+    }
+  },
+  methods: {
+    async getUserInfo() {
+      const res = await fetch('https://randomuser.me/api');
+      const { results } = await res.json();
+      this.firstName = results[0].name.first,
+      this.lastName  = results[0].name.last,
+      this.email     = results[0].email,
+      this.gender    = results[0].gender,
+      this.picture   = results[0].picture.large
     }
   }
 });
